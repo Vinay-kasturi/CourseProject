b75b94e1844708526db185c91fc81995ed7060e9
Vinay-kasturi <vinay.kasturi@gmail.com>
2021-12-16 23:02:45 +0000

    Final Code

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ df = pandas.read_excel("AircraftEventsDetail151221.xlsx")
 #print(df.isna().sum())
 
 #Dataframe cleaning
-cleaned_df=df[['Event Type','Aircraft Type','New Operator','New Manager','New Owner','New Engine Sub Series','New Operator Country','Delivery Date']]
+cleaned_df=df[['Event Type','Aircraft Type','New Operator','New Manager','New Owner','New Engine Type','New Operator Country','Delivery Date']]
 #print(cleaned_df.shape)
 print(cleaned_df.info())
 print(cleaned_df)
@@ -27,9 +27,7 @@ print(cleaned_indexed_df.info())
 X=Cleaned_sorted_df["Aircraft Type"].tolist()
 A = ["A320","A321"]
 N = [0,0]
-print(X)
-N1 = 0
-N2 = 0
+
 for i in X:
    if i == A[0]:
        N[0] = N[0]+1
@@ -38,5 +36,22 @@ for i in X:
 print(N[0])
 print(N[1])
 
-ax.bar(A,N)
+#ax.bar(A,N)
+
+#Analysing the Engine type - PW1100 or LEAP type
+X1=Cleaned_sorted_df["New Engine Type"].tolist()
+E = ["LEAP","PW1000G","Unannounced"]
+N1 = [0,0,0]
+
+for i in X1:
+   if i == E[0]:
+       N1[0] = N1[0]+1
+   elif i == E[1]:
+       N1[1] = N1[1]+1
+   else:
+    N1[2] = N1[2]+1
+print(N1[0])
+print(N1[1])
+print(N1[2])
+ax.bar(E,N1)
 plt.show()
